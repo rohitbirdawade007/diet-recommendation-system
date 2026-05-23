af01dd86408263fef0ecdc285f2b4275be5b1c1d
Rohit Birdawade <rohitbirdawade2875@gmail.com>
2026-05-23 22:38:33 +0530

    feat(config): add dataset file path

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 ﻿import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR  = os.path.join(BASE_DIR, 'data')
+BASE_DIR   = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR   = os.path.join(BASE_DIR, 'data')
 MODELS_DIR = os.path.join(BASE_DIR, 'models')
+
+DATA_FILE = 'diet_recommendation_dataset_1000.csv'
+DATA_PATH = os.path.join(DATA_DIR, DATA_FILE)
